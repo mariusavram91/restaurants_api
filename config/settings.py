52f3dd2792404f80659807fb0d092a0233fdc272
Marius Avram <me@mariusavram.com>
2018-09-17 21:06:37 +0200

    Rename restaurants to config

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'restaurants.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'restaurants.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
